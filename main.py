146b1874d48961d14c7d50b3a12340fa363729f6
TheOnlyRealRyan <ryansputzier@gmail.com>
2024-05-05 21:57:26 +0200

    new yt module, ffmpeg module

--- main.py
+++ main.py
@@ -1,13 +1,122 @@
-from pyyoutube import Api
-# api = Api(api_key="")
-api = Api(access_token='')
-api = Api(client_id="", client_secret="")
-print(api.get_authorization_url())
-api.generate_access_token(authorization_response="https://accounts.google.com/o/oauth2/v2/auth?response_type=code&client_id=994151002990-lbkfnunnbh7646fagac1r48k7t5rusut.apps.googleusercontent.com&redirect_uri=https%3A%2F%2Flocalhost%2F&scope=https%3A%2F%2Fwww.googleapis.com%2Fauth%2Fyoutube+https%3A%2F%2Fwww.googleapis.com%2Fauth%2Fuserinfo.profile&state=PyYouTube&access_type=offline&prompt=select_account")
-# api = Api(api_key="")
-# api = Api(access_token='')
-# print(channel_by_id)
-# print(channel_by_id.items)
-# print(channel_by_id.items[0].to_dict())
-# laylists_by_mine = api.get_playlists(mine=True)
-# print(playlists_by_mine)
+from pyyoutube import Client
+from pytube import Playlist
+from pytube import YouTube
+import ffmpeg
+import re
+import os
+import fnmatch 
+
+
+def main():
+    # playlists()
+    combine_audio_and_video()
+    # download_playlist()
+    # download_video()
+    
+    
+def combine_audio_and_video() -> None:
+    video_directory = "./video_track"
+    filePattern = "*.*"
+    #TODO: Ping pong between directories and combine tracks
+
+    # Find Audio track
+    audio_directory = "./audio_track"
+    for path, dirs, files in os.walk(os.path.abspath(audio_directory)):
+        for afilename in fnmatch.filter(files, filePattern):
+            
+            aud = afilename.strip("audio_")
+
+            print(afilename)
+            print(aud)
+
+            # TODO: Compare stripped aud and vid and then combine the two if equal
+            """
+            if len(aud)> 0:
+                input_audio = ffmpeg.input(f'./downloads/{afilename}')
+                # print("-->" +  aud[0])
+            
+                # Find the matching video file
+                for path, dirs, files in os.walk(os.path.abspath(video_directory)):
+                    for vfilename in fnmatch.filter(files, filePattern):
+                        # print(filename)
+                        vid = re.findall('video_*', vfilename)
+                        if len(vid)> 0:
+                            input_video = ffmpeg.input(f'./downloads/{vfilename}')
+                            # print(vid)
+                    print(afilename)
+                    print(vfilename)
+                    if afilename == vfilename:
+                        print("here")   
+                        
+            """
+      
+    """
+    ffmpeg.concat(input_video, input_audio, v=1, a=1).output('./processed_folder/finished_video.mp4').run()            
+    """            
+                
+    """
+    input_video = ffmpeg.input(re.findall('./downloads/video_(*).mp4'))
+
+    input_audio = ffmpeg.input(re.findall('./downloads/audio_(*).(*))'))
+
+    ffmpeg.concat(input_video, input_audio, v=1, a=1).output('./processed_folder/finished_video.mp4').run()
+    """
+
+
+def display_playlists() -> None:
+    url = "https://www.youtube.com/playlist?list=PLmwmioLhtgmBc-ScvtbTXaWe27icfy4Kf"
+    
+    try: 
+        playlist = Playlist(url) 
+        print('Number of videos in playlist: %s' % len(playlist.video_urls))
+
+        for urls in playlist.video_urls:
+            print(f"url: {urls}")
+            yt = YouTube(urls)
+            print(yt.vid_info["videoDetails"]["title"]) 
+            # print(yt.vid_info["videoDetails"])
+    except Exception as e: 
+        print(f"Error {str(e)}")
+    
+
+def download_playlist() -> None:
+    video_path = "./video_track"
+    audio_path = "./audio_track"
+    # Download all videos in a playlist
+    url = "https://www.youtube.com/playlist?list=PLmwmioLhtgmBc-ScvtbTXaWe27icfy4Kf"
+
+    try:
+        playlist = Playlist(url)
+        print('Number of videos in playlist: %s' % len(playlist.video_urls))
+        print(f'-->Downloading: {playlist.title}')
+        # print(f'Downloading: {playlist.title}')
+        for video in playlist.videos:
+            # print(video.title)
+            print(f"-->Downloading: {video.title}")
+            # print(f"Resolution: {video.streams.get_highest_resolution()}")
+            # video.streams.get_highest_resolution().download(path)
+            
+            
+            print(f"Resolution: {video.streams.filter(progressive=False, file_extension='mp4').order_by('resolution').desc().first()}")
+            video.streams.filter(progressive=False, file_extension='mp4').order_by('resolution').desc().first().download(filename_prefix="video_", output_path=video_path)
+            video.streams.filter(only_audio=True, progressive=False).desc().first().download(filename_prefix="audio_", output_path=audio_path)
+
+    except Exception as e: 
+        print(f"Error {str(e)}")
+    
+        
+def download_video() -> None:
+    
+    url = "https://www.youtube.com/watch?v=0hEmxOEeVO0"
+    resolution = "720p"  #  "720p" "480p" "1080p"
+    try: 
+        yt = YouTube(url) 
+        stream = yt.streams.filter(res=resolution).first() 
+        print(yt.title)
+        # stream.download() 
+    except Exception as e: 
+        print(f"Error {str(e)}")
+
+    
+if __name__ == "__main__":
+    main()
